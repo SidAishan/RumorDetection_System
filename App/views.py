825b13f8ac7179047cde45f10778346c8b3d22b7
劉愛 <liuaishan@ryuuaisugis-MacBook-Air.local>
2020-10-27 16:11:28 +0800

    Update to the final version

--- App/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def hello(request):
-    return HttpResponse("<h1>hhhh</h1>")
-
-def index(request):
-    return render(request, 'index.html')
